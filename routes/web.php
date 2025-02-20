1aa7275a53fbcdc20b739445d51a79bd8f8e414d
NaufalArdian12 <naufalopo87@gmail.com>
2025-02-20 21:14:03 +0700

    feat: Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,6 +36,15 @@ Route::get('/', [HomeController::class,'index']);
 
 Route::get('/about', [AboutController::class,'about']);
 Route::get('/articles/{id}', [ArticleController::class,'articles']);
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([ 'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([ 'create', 'store', 'update', 'destroy'
+]);
+
 
 // Route::get('/user/{name?}', function ($name=null) {
 // return 'My name is '.$name;
