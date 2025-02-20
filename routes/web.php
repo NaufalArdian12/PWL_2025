aa8c366410e1669c9eb9dec8b045ce0726e4060b
NaufalArdian12 <naufalopo87@gmail.com>
2025-02-20 15:17:43 +0700

    feat: Route Name

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,34 +15,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'welcome';
-});
+// Route::get('/', function () {
+//     return 'welcome';
+// });
 
-Route::get('about', function () {
-    return 'Hello, My Name Nopal <br> 2341760148';
-});
+// Route::get('about', function () {
+//     return 'Hello, My Name Nopal <br> 2341760148';
+// });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
 
-Route::get('/world', function () {
-    return 'World';
-});
+// Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/', [PageController::class,'index']);
+
+Route::get('/about', [PageController::class,'about']);
+Route::get('/articles/{id}', [PageController::class,'articles']);
 
 // Route::get('/user/{name?}', function ($name=null) {
 // return 'My name is '.$name;
 // });
 
-Route::get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-});
+// Route::get('/user/{name?}', function ($name='John') {
+//     return 'Nama saya '.$name;
+// });
 
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
+// Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+//     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+// });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Article Page with ID '.$id;
-});
+// Route::get('/articles/{id}', function ($id) {
+//     return 'Article Page with ID '.$id;
+// });
