8214ff54778a2686a7d9782f94e3a87c13e8dcbc
NaufalArdian12 <naufalopo87@gmail.com>
2025-02-20 14:39:33 +0700

    feat: Route Parameters

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'My name is '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Article Page with ID '.$id;
+});
